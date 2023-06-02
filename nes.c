16b12312b93a56a755aa5f5652e2a9cce49d8626
ncy <nongchangyin@xd.com>
2023-06-02 20:14:40 +0800

    -

--- nes.c
+++ nes.c
@@ -50,9 +50,9 @@ typedef enum{
     NOP,BRK,PHP,BPL,CLC,ORA,STP,ASL,SLO,ANC,
     JSR,BIT,PLP,BMI,SEC,AND,ROL,RLA,
     RTI,PHA,JMP,BVC,CLI,EOR,LSR,SRE,ALR,
-    RTS,PLA,BVS,SEI,ADC,ROR,RRA,
+    RTS,PLA,BVS,SEI,ADC,ROR,ARR,RRA,
     STY,DEY,BCC,TYA,SHY,STA,STX,TXA,TXS,SHX,SAX,XAA,AHX,TAS,
-    LDY,TAY,BCS,CLV,LDA,LDX,TAX,TSX,
+    LDY,TAY,BCS,CLV,LDA,LDX,LAX,LAS,TAX,TSX,
     CPY,INY,BNE,CLD,CMP,DEC,DEX,DCP,AXS,
     CPX,INX,BEQ,SED,SBC,INC,ISC,
 }inst_type_t;
@@ -72,13 +72,14 @@ static char * inst_tag[] = {
     "nop","brk","php","bpl","clc","ora","stp","asl","slo","anc",
     "jsr","bit","plp","bmi","sec","and","rol","rla",
     "rti","pha","jmp","bvc","cli","eor","lsr","sre","alr",
-    "rts","pla","bvs","sei","adc","ror","rra",
+    "rts","pla","bvs","sei","adc","ror","arr","rra",
     "sty","dey","bcc","tya","shy","sta","stx","txa","txs","shx","sax","xaa","ahx","tas",
-    "ldy","tay","bcs","clv","lda","ldx","tax","tsx",
+    "ldy","tay","bcs","clv","lda","ldx","lax","las","tax","tsx",
     "cpy","iny","bne","cld","cmp","dec","dex","dcp","axs",
     "cpx","inx","beq","sed","sbc","inc","isc"
 };
 
+/* http://www.oxyron.de/html/opcodes02.html */ 
 static inst_t inst_map[] = {
     {BRK, IMP, 7}, 
     {ORA, IZX, 6}, 
@@ -126,6 +127,289 @@ static inst_t inst_map[] = {
     {AND, IZX, 6},
     {STP, IMP, 0},
     {RLA, IZX, 8},
+
+    {BIT, ZP, 3},
+    {AND, ZP, 3},
+    {ROL, ZP, 5},
+    {RLA, ZP, 5},
+
+    {PLP, IMP, 4},
+    {AND, IMM, 2},
+    {ROL, IMP, 2},
+    {ANC, IMM, 2},
+
+    {BIT, ABS, 4},
+    {AND, ABS, 4},
+    {ROL, ABS, 6},
+    {RLA, ABS, 6},
+
+
+    {BMI, REL, 2},
+    {AND, IZY, 5},
+    {STP, IMP, 0},
+    {RLA, IZY, 8},
+
+    {NOP, ZPX, 4},
+    {AND, ZPX, 4},
+    {ROL, ZPX, 6},
+    {RLA, ZPX, 6},
+
+    {SEC, IMP, 2},
+    {AND, ABY, 4},
+    {NOP, IMP, 2},
+    {RLA, ABY, 7},
+
+    {NOP, ABX, 4},
+    {AND, ABX, 4},
+    {ROL, ABX, 7},
+    {RLA, ABX, 7},
+
+
+    {RTI, IMP, 6},
+    {EOR, IZX, 6},
+    {STP, IMP, 0},
+    {SRE, IZX, 8},
+
+    {NOP, ZP, 3},
+    {EOR, ZP, 3},
+    {LSR, ZP, 5},
+    {SRE, ZP, 5},
+
+    {PHA, IMP, 3},
+    {EOR, IMM, 2},
+    {LSR, IMP, 2},
+    {ALR, IMM, 2},
+
+    {JMP, ABS, 3},
+    {EOR, ABS, 4},
+    {LSR, ABS, 6},
+    {SRE, ABS, 6},
+
+
+    {BVC, REL, 2},
+    {EOR, IZY, 5},
+    {STP, IMP, 0},
+    {SRE, IZY, 8},
+
+    {NOP, ZPX, 4},
+    {EOR, ZPX, 4},
+    {LSR, ZPX, 6},
+    {SRE, ZPX, 6},
+
+    {CLI, IMP, 2},
+    {EOR, ABY, 4},
+    {NOP, IMP, 2},
+    {SRE, ABY, 7},
+
+    {NOP, ABX, 4},
+    {EOR, ABX, 4},
+    {LSR, ABX, 7},
+    {SRE, ABX, 7},
+
+
+    //6x
+    {RTS, IMP, 6},
+    {ADC, IZX, 6},
+    {STP, IMP, 0},
+    {RRA, IZX, 8},
+
+    {NOP, ZP, 3},
+    {ADC, ZP, 3},
+    {ROR, ZP, 5},
+    {RRA, ZP, 5},
+
+    {PLA, IMP, 4},
+    {ADC, IMM, 2},
+    {ROR, IMP, 2},
+    {ARR, IMM, 2},
+
+    {JMP, IND, 5},
+    {ADC, ABS, 4},
+    {ROR, ABS, 6},
+    {RRA, ABS, 6},
+
+    //7x
+    {BVS, REL, 2},
+    {ADC, IZY, 5},
+    {STP, IMP, 0},
+    {RRA, IZY, 8},
+
+    {NOP, ZPX, 4},
+    {ADC, ZPX, 4},
+    {ROR, ZPX, 6},
+    {RRA, ZPX, 6},
+
+    {SEI, IMP, 2},
+    {ADC, ABY, 4},
+    {NOP, IMP, 2},
+    {RRA, ABY, 7},
+
+    {NOP, ABX, 4},
+    {ADC, ABX, 4},
+    {ROR, ABX, 7},
+    {RRA, ABX, 7},
+
+    //8x
+    {NOP, IMM, 2},
+    {STA, IZX, 6},
+    {NOP, IMM, 2},
+    {SAX, IZX, 6},
+
+    {STY, ZP, 3},
+    {STA, ZP, 3},
+    {STX, ZP, 3},
+    {SAX, ZP, 3},
+
+    {DEY, IMP, 2},
+    {NOP, IMM, 2},
+    {TXA, IMP, 2},
+    {XAA, IMM, 2},
+
+    {STY, ABS, 4},
+    {STA, ABS, 4},
+    {STX, ABS, 4},
+    {SAX, ABS, 4},
+
+    //9x
+    {BCC, REL, 2},
+    {STA, IZY, 6},
+    {STP, IMP, 0},
+    {AHX, IZY, 6},
+
+    {STY, ZPX, 4},
+    {STA, ZPX, 4},
+    {STX, ZPY, 4},
+    {SAX, ZPY, 4},
+
+    {TYA, IMP, 2},
+    {STA, ABY, 5},
+    {TXS, IMP, 2},
+    {TAS, ABY, 5},
+
+    {SHY, ABX, 5},
+    {STA, ABX, 5},
+    {SHX, ABY, 5},
+    {AHX, ABY, 5},
+
+    {LDY, IMM, 2},
+    {LDA, IZX, 6},
+    {LDX, IMM, 2},
+    {LAX, IZX, 6},
+
+    {LDY, ZP, 3},
+    {LDA, ZP, 3},
+    {LDX, ZP, 3},
+    {LAX, ZP, 3},
+
+    {TAY, IMP, 2},
+    {LDA, IMM, 2},
+    {TAX, IMP, 2},
+    {LAX, IMM, 2},
+
+    {LDY, ABS, 4},
+    {LDA, ABS, 4},
+    {LDX, ABS, 4},
+    {LAX, ABS, 4},
+
+    {BCS, REL, 2},
+    {LDA, IZY, 5},
+    {STP, IMP, 0},
+    {LAX, IZY, 5},
+
+    {LDY, ZPX, 4},
+    {LDA, ZPX, 4},
+    {LDX, ZPY, 4},
+    {LAX, ZPY, 4},
+
+    {CLV, IMP, 2},
+    {LDA, ABY, 4},
+    {TSX, IMP, 2},
+    {LAS, ABY, 4},
+
+    {LDY, ABX, 4},
+    {LDA, ABX, 4},
+    {LDX, ABY, 4},
+    {LAX, ABY, 4},
+
+    {CPY, IMM, 2},
+    {CMP, IZX, 6},
+    {NOP, IMM, 2},
+    {DCP, IZX, 8},
+
+    {CPY, ZP, 3},
+    {CMP, ZP, 3},
+    {DEC, ZP, 5},
+    {DCP, ZP, 5},
+
+    {INY, IMP, 2},
+    {CMP, IMM, 2},
+    {DEX, IMP, 2},
+    {AXS, IMM, 2},
+
+    {CPY, ABS, 4},
+    {CMP, ABS, 4},
+    {DEC, ABS, 6},
+    {DCP, ABS, 6},
+
+    {BNE, REL, 2},
+    {CMP, IZY, 5},
+    {STP, IMP, 0},
+    {DCP, IZY, 8},
+
+    {NOP, ZPX, 4},
+    {CMP, ZPX, 4},
+    {DEC, ZPX, 6},
+    {DCP, ZPX, 6},
+
+    {CLD, IMP, 2},
+    {CMP, ABY, 4},
+    {NOP, IMP, 2},
+    {DCP, ABY, 7},
+
+    {NOP, ABX, 4},
+    {CMP, ABX, 4},
+    {DEC, ABX, 7},
+    {DCP, ABX, 7},
+
+    {CPX, IMM, 2},
+    {SBC, IZX, 6},
+    {NOP, IMM, 2},
+    {ISC, IZX, 8},
+
+    {CPX, ZP, 3},
+    {SBC, ZP, 3},
+    {INC, ZP, 5},
+    {ISC, ZP, 5},
+
+    {INX, IMP, 2},
+    {SBC, IMM, 2},
+    {NOP, IMP, 2},
+    {SBC, IMM, 2},
+
+    {CPX, ABS, 4},
+    {SBC, ABS, 4},
+    {INC, ABS, 6},
+    {ISC, ABS, 6},
+
+    {BEQ, REL, 2},
+    {SBC, IZY, 5},
+    {STP, IMP, 0},
+    {ISC, IZY, 8},
+
+    {NOP, ZPX, 4},
+    {SBC, ZPX, 4},
+    {INC, ZPX, 6},
+    {ISC, ZPX, 6},
+
+    {SED, IMP, 2},
+    {SBC, ABY, 4},
+    {NOP, IMP, 2},
+    {ISC, ABY, 7},
+
+    {NOP, ABX, 4},
+    {SBC, ABX, 4},
+    {INC, ABX, 7},
+    {ISC, ABX, 7}
 };
 
 typedef struct {
@@ -140,14 +424,20 @@ typedef struct {
     uint8_t * ram;
     uint8_t inst;
     uint16_t abs;
-    uint16_t rel;
+    int8_t rel;
     uint8_t operand;
 }cpu_t;
 
 typedef struct {
+    uint8_t ppu_ctrl;
+    uint8_t ppu_mask;
+    uint8_t ppu_status;
+    uint8_t oam_addr;
+    uint8_t ppu_scroll;
+    uint8_t ppu_addr;
+    uint8_t ppu_data;
+    uint8_t oamdma;
 }ppu_t;
-
-
 
 typedef struct {
     cpu_t * cpu;
@@ -371,16 +661,15 @@ void cpu_power_up(cpu_t * cpu)
 void cpu_interupt(cpu_t * cpu, cpu_int_t i)
 {
     if (i == IRQ && bit_get(&cpu->p,I)) {
+        printf("ignore irq\n");
         return;
     }
 
     bit_set(&cpu->p, I);
     uint16_t vector = 0xfffa + i * 2;
-    printf("vector:%x\n", vector);
     cpu_stack_push2(cpu, cpu->pc);
     cpu_stack_push(cpu, cpu->p);
     cpu->pc = bus_read2(cpu->bus, vector);
-    printf("pc:%x\n", cpu->pc);
     cpu->cycles += 7;
 }
 
@@ -397,66 +686,112 @@ uint16_t cpu_fetch2(cpu_t * cpu)
 
 uint8_t cpu_fetch_inst(cpu_t * cpu)
 {
+    printf("$%04x ", cpu->pc);
     cpu->inst = cpu_fetch(cpu);
 
     inst_t * inst = inst_map + cpu->inst;
-    printf("$%04x %s ", cpu->pc, inst_tag[inst->t], inst->am, inst->cycles);
+    cpu->cycles += inst->cycles;
+    printf("%02x %s ", cpu->inst, inst_tag[inst->t], inst->am, inst->cycles);
     if (inst->am == IMP) {
     }else if (inst->am == IMM) {
-        cpu->data = cpu_fetch(cpu);
-        printf("#$%02x", cpu->data);
+        cpu->operand = cpu_fetch(cpu);
+        printf("#$%02x", cpu->operand);
     }else if (inst->am == ZP) {
         uint8_t zp = cpu_fetch(cpu);
-        cpu->data = bus_read(cpu->bus, zp);
+        cpu->abs = zp;
+        cpu->operand = bus_read(cpu->bus, zp);
         printf("$%02x", zp);
     }else if (inst->am == ZPX) {
         uint8_t zp = cpu_fetch(cpu);
-        cpu->data = bus_read(cpu->bus, zp+cpu->x);
+        cpu->abs = zp + cpu->x;
+        cpu->operand = bus_read(cpu->bus, zp+cpu->x);
         printf("$%02x,X[%x]", zp, cpu->x);
     }else if (inst->am == ZPY) {
         uint8_t zp = cpu_fetch(cpu);
-        cpu->data = bus_read(cpu->bus, zp+cpu->y);
+        cpu->abs = zp + cpu->y;
+        cpu->operand = bus_read(cpu->bus, zp+cpu->y);
         printf("$%02x,Y[%x]", zp, cpu->y);
     }else if (inst->am == IZX) {
         uint8_t z = cpu_fetch(cpu);
         uint8_t ptr = bus_read(cpu->bus, z + cpu->x); 
-        cpu->data = bus_read(cpu->bus, ptr);
+        cpu->operand = bus_read(cpu->bus, ptr);
         printf("($%02x,X[%x])", z, cpu->x);
     }else if (inst->am == IZY) {
         uint8_t z = cpu_fetch(cpu);
         uint8_t ptr = bus_read(cpu->bus, z + cpu->y); 
-        cpu->data = bus_read(cpu->bus, ptr);
+        cpu->operand = bus_read(cpu->bus, ptr);
         printf("($%02x,Y[%x])", z, cpu->y);
     }else if (inst->am == ABS) {
         uint16_t ptr = cpu_fetch2(cpu);
-        cpu->data = bus_read(cpu->bus, ptr);
+        cpu->abs = ptr;
+        cpu->operand = bus_read(cpu->bus, ptr);
         printf("$%04x", ptr);
     }else if (inst->am == ABX) {
         uint16_t ptr = cpu_fetch2(cpu);
-        cpu->data = bus_read(cpu->bus, ptr+cpu->x);
+        cpu->operand = bus_read(cpu->bus, ptr+cpu->x);
         printf("$%04x,X[%x]", ptr, cpu->x);
     }else if (inst->am == ABY) {
         uint16_t ptr = cpu_fetch2(cpu);
-        cpu->data = bus_read(cpu->bus, ptr+cpu->y);
+        cpu->operand = bus_read(cpu->bus, ptr+cpu->y);
         printf("$%04x,Y[%x]", ptr, cpu->y);
     }else if (inst->am == IND) {
         uint16_t ptr = cpu_fetch2(cpu);
         uint16_t real_ptr = bus_read2(cpu->bus, ptr);
-        cpu->data = bus_read(cpu->bus, real_ptr);
+        cpu->operand = bus_read(cpu->bus, real_ptr);
         printf("($%04x)", ptr);
     }else if (inst->am == REL) {
-        uint16_t offset = cpu_fetch2(cpu);
-        cpu->data = bus_read(cpu->bus, cpu->pc - offset);
-        printf("pc-%x", offset);
+        int8_t offset = cpu_fetch(cpu);
+        cpu->rel = offset;
+        cpu->operand = bus_read(cpu->bus, cpu->pc - offset);
+        printf("pc-%x", offset & 0xff);
     }
 
-    printf("\n");
     return 0;
 }
 
 uint8_t cpu_exec(cpu_t * cpu)
 {
-    switch(cpu->inst) {
+    printf("\n");
+    if (cpu->inst > sizeof(inst_map)/sizeof(inst_t)) {
+        return 0;
+    }
+
+    inst_t * inst = inst_map + cpu->inst;
+    switch(inst->t) {
+        default:
+            printf("un supported instruction %x\n", cpu->inst);
+            break;
+        case JSR:
+            cpu_stack_push2(cpu, cpu->pc);
+            cpu->pc = cpu->abs;
+            break;
+        case INC:
+            bus_write(cpu->bus, cpu->abs, cpu->operand+1);
+            break;
+        case DEC:
+            bus_write(cpu->bus, cpu->abs, cpu->operand-1);
+            break;
+        case LDA:
+            cpu->a = bus_read(cpu->bus, cpu->abs);
+            break;
+        case BPL:
+            cpu->pc = cpu->pc + cpu->rel * (bit_get(&cpu->p, N) == 0);
+            break;
+        case BRK:
+            bit_clr(&cpu->p, I);
+            bit_set(&cpu->p, B);
+            cpu_interupt(cpu, IRQ);
+            break;
+        case RTI:
+            cpu->p = cpu_stack_pop(cpu);
+            cpu->pc = cpu_stack_pop2(cpu);
+            break;
+        case RTS:
+            cpu->pc = cpu_stack_pop2(cpu);
+            break;
+        case STA:
+            bus_write(cpu->bus, cpu->abs, cpu->a);
+            break;
     }
     return 0;
 }
@@ -464,10 +799,28 @@ uint8_t cpu_exec(cpu_t * cpu)
 void cpu_clock(cpu_t * cpu)
 {
     if (cpu->cycles == 0) {
+
+        printf("pc:$%04x a:%x x:%x y:%x N:%d V:%d B:%d D:%d I:%d Z:%d C:%d ", cpu->pc, cpu->a, cpu->x, cpu->y, 
+            bit_get(&cpu->p, N),
+            bit_get(&cpu->p, V),
+            bit_get(&cpu->p, B),
+            bit_get(&cpu->p, D),
+            bit_get(&cpu->p, I),
+            bit_get(&cpu->p, Z),
+            bit_get(&cpu->p, C)
+            );
         cpu->cycles += cpu_fetch_inst(cpu);
         cpu->cycles += cpu_exec(cpu);
     }
     cpu->cycles --;
+}
+
+ppu_t * ppu_create()
+{
+    ppu_t * ppu = (ppu_t*)malloc(sizeof(ppu_t));
+    memset(ppu, 0, sizeof(ppu_t));
+
+    return ppu;
 }
 
 nes_t * nes_create()
@@ -475,6 +828,7 @@ nes_t * nes_create()
     nes_t * nes = (nes_t*)malloc(sizeof(nes_t));
     memset(nes, 0, sizeof(nes_t));
     nes->cpu = cpu_create();
+    nes->ppu = ppu_create();
     return nes;
 }
 
@@ -517,7 +871,7 @@ int main(int argc, char * argv[])
     nes_t * nes = nes_create();
     nes_power_up(nes);
     nes_set_rom(nes,&rom);
-    for (int i = 0 ; i < 10; ++ i) {
+    for (int i = 0 ; i < 1000; ++ i) {
         nes_clock(nes);
     }
     return 0;
